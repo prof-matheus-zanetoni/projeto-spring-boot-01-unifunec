e9268d7d325d9260f1fc020cf84b32dced54e8c1
Prof. Matheus Galbero Zanetoni <mgzanetoni@funecsantafe.edu.br>
2023-08-23 23:46:03 -0300

    Configurando a aplicação Spring Boot (criando classe model de mensagem da API)

---
+++ src/main/java/br/com/calculadora/projetocalculadora/ProjetocalculadoraApplication.java
@@ -0,0 +1,13 @@
+package br.com.calculadora.projetocalculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetocalculadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetocalculadoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/calculadora/projetocalculadora/ProjetocalculadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.calculadora.projetocalculadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetocalculadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
